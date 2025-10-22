ffc0ae18dfd52c7ef1efd0a539421a633ffb90c6
Ian Perez <ianaxelperezdelatorre@gmail.com>
2025-10-22 01:20:20 -0600

    Initial Commit

---
+++ MyCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyCalculator
+//
+//  Created by Ian Axel Perez de la Torre on 22/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
